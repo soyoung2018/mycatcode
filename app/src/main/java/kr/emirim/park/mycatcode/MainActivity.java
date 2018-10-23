9fc427fa83690fe13a8473f254f5c4d557430367
HunkyoungPark <soyong20@naver.com>
2018-10-23 20:10:34 +0900

    프로젝트 생성

---
+++ app/src/main/java/kr/emirim/park/mycatcode/MainActivity.java
@@ -0,0 +1,13 @@
+package kr.emirim.park.mycatcode;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
